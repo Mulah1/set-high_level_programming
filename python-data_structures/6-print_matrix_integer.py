23f77b77d81dc73887f6618db97af811ec6ba56a
Frank <frankmulah250@gmail.com>
2026-09-23 11:01:29 +0200

    Handle empty rows in print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
     for row in matrix:
+        if not row:
+            print()
+            continue
         for index, number in enumerate(row):
             if index == len(row) - 1:
                 print("{:d}".format(number))
