f4a9f8b041a5bd7cf81ff9a27b127b599c1ea302
Frank <frankmulah250@gmail.com>
2026-09-23 12:26:29 +0200

    Complete Python import modules exercises 0 through 103.

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in sorted(names):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
